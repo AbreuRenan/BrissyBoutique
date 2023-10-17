e9001bd87d67a1db8b71721a0ef869f25e97becd
AbreuRenan <renan.de.abreu@gmail.com>
2023-10-17 02:46:44 -0300

    primeira etapa de consulta a Graph API finalizada, dados de midias retornando com sucesso

---
+++ src/components/InstaFeed/ReactFeedPlugin.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React from "react";
+
+function ReactFeedPlugin() {
+  const [instagramData, setInstagramData] = React.useState([]);
+  async function getInstaFeed() {
+    const token = import.meta.env.VITE_REACT_APP_FEED_PLUGIN_TOKEN;
+    const fields = "id,media_type,media_url,permalink";
+    const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=${fields}`;
+    const { data } = await axios.get(url);
+    setInstagramData([instagramData, ...data["data"]]);
+  }
+
+  React.useEffect(() => {
+    getInstaFeed();
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 1fr)",
+      }}
+    >
+      {instagramData
+        ?.filter((item) => item.media_type === "IMAGE")
+        .map((img) => {
+          return (
+            <div key={img.id}>
+              <img
+                src={img.media_url}
+                frameBorder="0"
+                width={"auto"}
+                height={"200px"}
+              />
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default ReactFeedPlugin;
